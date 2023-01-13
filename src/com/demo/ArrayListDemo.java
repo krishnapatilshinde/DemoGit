f761904e24f11933374b8e35a7cb15afdc434b45
Krishna <krishnapatil96@gmail.com>
2023-01-13 14:22:04 +0530

    first commit : added arraylist code

---
+++ src/com/demo/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList arraylist=new ArrayList();
+		arraylist.add(1);
+		arraylist.add(2);
+		arraylist.add(3);
+		
+		
+		System.out.println(arraylist);
+		
+		
+	}
+
+}
