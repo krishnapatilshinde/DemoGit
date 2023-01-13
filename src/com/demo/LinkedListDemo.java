e656b0847c6fbefd66fbb961a9561358ee8b020e
Krishna <krishnapatil96@gmail.com>
2023-01-13 14:42:22 +0530

    second commit : added linkedlist

---
+++ src/com/demo/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	
+	public static void main(String[] args) {
+		
+		LinkedList<String> linkedlist=new LinkedList<String>();
+		linkedlist.add("krishna");
+		linkedlist.add("pooja");
+		
+		System.out.println(linkedlist);
+		
+	}
+
+}
